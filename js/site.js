4b27e13a868ae37ad31b364ecf9f91752af07867
MikailR <41162984+MikailR@users.noreply.github.com>
2026-08-20 13:25:49 -0400

    Bypass Pages cache when loading the one-liner
    Match the cache-busting idiom already used by now.js and feed.js.

--- js/site.js
+++ js/site.js
@@ -5,7 +5,8 @@
   var src = root.getAttribute("data-src") || "data/site.json";
   var thesis = root.querySelector("[data-site-thesis]");
 
-  fetch(src)
+  var url = src + (src.indexOf("?") >= 0 ? "&" : "?") + "t=" + Date.now();
+  fetch(url, { cache: "no-store" })
     .then(function (response) {
       if (!response.ok) throw new Error("Could not load site");
       return response.json();
